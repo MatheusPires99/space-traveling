5752437066f35bf7a9791eb623e624dacf5a2933
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-11 14:40:49 -0300

    feat: create Wrapper component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,17 @@
 import Link from 'next/link';
 
-import { Flex, Image, Link as ChakraLink } from '@chakra-ui/react';
+import { Image, Link as ChakraLink } from '@chakra-ui/react';
+
+import { Wrapper } from '../Wrapper';
 
 export function Header() {
   return (
-    <Flex
-      as="header"
-      w="100%"
-      maxWidth={700}
-      mx="auto"
-      h={105}
-      px="6"
-      align="center"
-    >
+    <Wrapper as="header" h={105} align="center">
       <Link href="/" passHref>
         <ChakraLink>
           <Image src="/logo.svg" alt="logo" w="60" />
         </ChakraLink>
       </Link>
-    </Flex>
+    </Wrapper>
   );
 }
---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+import { Flex, FlexProps } from '@chakra-ui/react';
+
+type WrapperProps = FlexProps & {
+  children: ReactNode;
+};
+
+export function Wrapper({ children, ...rest }: WrapperProps) {
+  return (
+    <Flex w="100%" maxWidth={700} mx="auto" px="6" {...rest}>
+      {children}
+    </Flex>
+  );
+}
